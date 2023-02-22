c5c40e6d1bcdbaaa452234b0f1868402182c664b
Eddy <eddyjohn.terosier@hotmail.com>
2023-02-22 15:35:06 +0100

    spread operator changements mineurs

--- TheSpreadOperator/script.js
+++ TheSpreadOperator/script.js
@@ -1,10 +1,17 @@
 "use strict";
 
-// Data needed for a later exercise
+/* Le code montre comment utiliser l'opérateur spread (...) 
+pour copier des tableaux, lier des tableaux multiples et même passer 
+des variables comme arguments. Cela fonctionnera également sur des objets, 
+même s'ils ne sont pas itérables. Enfin, cela nous montre comment créer des 
+copies d'objets, attribuer de nouvelles valeurs et comparer les anciennes valeurs 
+aux nouvelles. */
+
+// Définition de la variable flights qui est utilisée dans un exercice ultérieur
 const flights =
     "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
-// Data needed for first part of the section
+// Définition d'un objet restaurant contenant plusieurs propriétés, des tableaux et des fonctions
 const restaurant = {
     name: "Classico Italiano",
     location: "Via Angelo Tavanti 23, Firenze, Italy",
@@ -22,114 +29,78 @@ const restaurant = {
             close: 23,
         },
         sat: {
-            open: 0, // Open 24 hours
+            open: 0, // Ouvert 24h/24
             close: 24,
         },
     },
 
+    // Définition d'une fonction qui permet de passer commande
     order: function (starterIndex, mainIndex) {
         return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
     },
 
+    // Définition d'une fonction pour passer commande à livrer
     orderDelivery: function ({
         starterIndex = 1,
         mainIndex = 0,
         time = "20:00",
-        address = "Undefined",
+        adress,
     }) {
         console.log(
-            `Order received ! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+            `Order received ! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be received to ${adress} at ${time}`
         );
     },
 
-    orderPasta: function (ing1, ing2, ing3) {
+    orderPasta: function (ing1, ing2ing3) {
         console.log(
-            `Here is your delicious pasta with ${ing1},${ing2} and ${ing3}`
+            `Here is your delictious pasta with ${ing1}, ${ing2},${ing3}`
         );
     },
 };
 
+// Appel de la fonction orderDelivery de l'objet restaurant avec un objet en argument
 restaurant.orderDelivery({
-    time: "22:30",
-    address: "Via del Sole,21",
-    mainIndex: 2,
-    starterIndex: 2,
+    adress: "Via del Sol,21",
 });
-restaurant.orderDelivery({});
-
-// Spread Operator
 
 const arr = [7, 8, 9];
-const newArray = [1, 2, ...arr];
-console.log(newArray);
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
 
-console.log(...newArray);
+const goodArr = [1, 2, ...arr]; // Ca prend tout les éléments du tableau et les sort individuellement ici ca les place dans le tableau
+console.log(goodArr);
+console.log(...goodArr);
 
-const newMenu = [...restaurant.mainMenu, "Gnocci"];
+const newMenu = [...restaurant.mainMenu, "Gnocci"]; // On récupere les elements du tableau puis on ajoute 'gnocci'
 console.log(newMenu);
 
-// Copy Array
+// Créer des copies de tableau
 
 const mainMenuCopy = [...restaurant.mainMenu];
 
-// Join 2 Arrays
+// Lié deux tableaux ou plus
 
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
-console.log(...menu);
+console.log(menu);
 
-// Iterables: arrays,strings,maps,sets. NOT objects
+// Les itérables : tableaux, chaines, maps, sets mais pas des objets
+
 const str = "Jonas";
-const letters = [...str, "", "s."];
+const letters = [...str, " ", "S."];
 console.log(letters);
+
 console.log(...str);
+// console.log(`${...str} Scmedtmann`); Ne marche pas ! impossible sur les objets
 
-//Real world Experiment
+// Depuis ES6 Ca marche sur les objets meme s'ils ne sont pas itérable
 
-// const ingredients = [
-//     prompt("Let's make pasta !\nIngredient 1?"),
-//     prompt("Ingredient 2?"),
-//     prompt("Ingredient 3?"),
-// // ];
-// console.log(ingredients);
-// restaurant.orderPasta(...ingredients);
-
-// Objects
-
-const newRestaurant = { foundedIn: 1995, ...restaurant, founder: "Giuseppe" };
+const newRestaurant = { ...restaurant, founder: "Guiseppe", foundedIn: 1998 };
 console.log(newRestaurant);
 
-const restaurantCopy = { ...restaurant };
-restaurantCopy.name = "Ristorante Roma";
+// On fait une copie du tableau pour pouvoir modifié les propriétés du tableau sans toucher à l'original
+
+const restaurantCopy = {...restaurant};
+restaurantCopy.name = 'Ristorante Roma';
+
 console.log(restaurantCopy.name);
 console.log(restaurant.name);
-
-// const { name, openingHours, categories } = restaurant;
-// console.log(name, openingHours, categories);
-
-// const {
-//     name: restaurantName,
-//     openingHours: hours,
-//     categories: tags,
-// } = restaurant;
-// console.log(restaurantName, hours, tags);
-
-// // Default values
-
-// const { menu = [], starterMenu: starters = [] } = restaurant;
-// console.log(menu, starters);
-
-// // Mutating variables
-
-// let a = 111;
-// let b = 999;
-// const obj = { a: 23, b: 7, c: 14 };
-
-// ({ a, b } = obj);
-// console.log(a, b);
-
-// // Nested Object
-
-// const {
-//     fri: { open: o, close: c },
-// } = openingHours;
-// console.log(o, c);
