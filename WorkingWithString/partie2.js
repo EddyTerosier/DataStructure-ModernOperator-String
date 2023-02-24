f2eef6a9075b0228631fa42b17183d1c90d25f5f
Eddy <eddyjohn.terosier@hotmail.com>
2023-02-24 12:08:14 +0100

    WorkingWithString ends

--- WorkingWithString/partie2.js
+++ WorkingWithString/partie2.js
@@ -1,2 +1,61 @@
-'use strict'
+"use strict";
 
+//WORKING WITH STRINGS
+
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+// Fix capitalization in name
+
+const passenger = "jOnAS";
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect = passengerLower[0].toUpperCase()+passengerLower.slice(1); // Car lower et upper enlever le reste quand on spécifie un index
+console.log(passengerCorrect);
+
+const email = 'hello@jonas.io';
+const logingEmail = '  Hello@Jonas.Io \n';
+
+const lowerEmail = logingEmail.toLowerCase();
+const trimmedEmail = lowerEmail.trim(); // enleve les espaces au début et a la fin des chaines de caractere
+console.log(trimmedEmail);
+
+const normalizedEmail = logingEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+// Replacing
+
+const priceEUR = '288,97€';
+const priceUS = priceEUR.replace('€', '$').replace(',','.'); // replace prend deux arguments le 1er est le caractere a changé et l'autre celui qui remplace
+console.log(priceUS);
+
+const announcement = "All passengers come to boarding door 23. Boarding door 23!";
+
+console.log(announcement.replace('door', 'gate')); // ca remplace que le premier caractere qu'il rencontre
+console.log(announcement.replaceAll('door', 'gate')); // ca remplace tout les caracteres qu'il rencontrera
+
+// Booleans
+
+const plane1 = "Airbus A320neo";
+console.log(plane1.includes('A320')); // Pour demander si ca contient la chaine de caractere renvoie TRUE
+console.log(plane1.includes('Boeing')); // Renvoie FALSE
+console.log(plane1.startsWith('Air')); // Pour demander si ca commmence par la chaine de caractere renvoie TRUE
+
+if(plane1.startsWith('Airbus') && plane1.endsWith('neo')){
+    console.log('Part of the NEW Airbus family');
+};
+
+// Practice exercise
+
+const checkBaggage = function(items){
+    const baggage = items.toLowerCase();
+    if (baggage.includes('_') || baggage.includes('gun')) {
+        console.log('You are NOT allowed on board');
+    } else {
+        console.log('Welcome on board');
+    }
+};
+
+checkBaggage('I have a laptop, some food and a pocket _');
+checkBaggage('Socks and camera');
+checkBaggage('Got some snacks and a guns for protection');
--- WorkingWithString/partie3.js
+++ WorkingWithString/partie3.js
@@ -1,2 +1,50 @@
 'use strict'
 
+console.log('a+very+nice+string'.split('+')); // sépare les éléments par les "+" et les places dans un tableau
+console.log('Jonas Schmedtmann'.split(' ')); // sépare les éléments par les " "
+
+const [firstName, lastName] = 'Jonas Schmedtmann'.split(' ');
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' '); // la méthode "join()" les relie par les caracteres mis en argument ici des espaces " " et les sort du tableau
+console.log(newName);
+
+const capitalizedName = function(name){
+    const names = name.split(' ');
+    const namesUpper = [];
+
+    for (const n of names) {
+        // namesUpper.push(n[0].toUpperCase()+n.slice(1)) // old method
+        namesUpper.push(n.replace(n[0],n[0].toUpperCase())) // new method
+    }
+    console.log(namesUpper.join(' '));
+}
+
+capitalizedName('jessica ann smith davis');
+capitalizedName('eddy el hombre de la manana');
+
+// Padding/Remplissage
+
+const message = "Go to gate 23!";
+console.log(message.padStart(25, '+')); // Retourne une chaine de caractere avec 25 caracteres en remplissant le début avec ce qu'on lui dit ici des "+"
+console.log(message.padEnd(35, "+")); // Retourne une chaine de caractere avec 35 caracteres en remplissant la fin avec ce qu'on lui dit ici des "+"
+
+const maskCreditCard = function(number){
+    const str = number + ''; // Si on additionne avec une chaine vide ca devient une string auto
+    const last = str.slice(-4);
+    return last.padStart(str.length, '*')
+};
+
+console.log(maskCreditCard(433784864647384));
+console.log(maskCreditCard('433784864647385'));
+
+// Repeat
+
+const message2 = 'Bad weather... All departures delayed... ';
+console.log(message2.repeat(5));
+
+const planesInLine = function(n){
+    console.log(`There are ${n} planes in line ${'✈'.repeat(n)}`);
+};
+
+planesInLine(5);
+planesInLine(4);
+planesInLine(7);
